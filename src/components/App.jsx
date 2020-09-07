7faf0c5e1a80df23ca0c75428385b89002662b3f
Saad-Siddiqui27 <52618232+Saad-Siddiqui27@users.noreply.github.com>
2020-09-07 15:05:09 +1200

    Added extra functionality that utilises the concept of Functional programming in order to loop over the notes objects file and render them automatically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer"
 import Note from  "./Note"
+import notes from "../notes"
+
+
+function createNotes(props){
+
+    return(
+        <Note
+           title= {props.title}
+           content = {props.title}
+        />
+    )
+}
 
 
 function App(){
     return <div>
        <Header/>
+       {notes.map(createNotes)}
        <Footer/>
-       <Note/>
+       
 
     </div>
 }
