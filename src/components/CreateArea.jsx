fc81462aa55b1e9320c97fa7c4fa3197ba3a6f9e
Saad-Siddiqui27 <52618232+Saad-Siddiqui27@users.noreply.github.com>
2020-09-08 14:15:45 +1200

    used the materialUI package to add buttons and did some state based rendering of UI components

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react"
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Fab from "@material-ui/core/Fab";  
+import Zoom from '@material-ui/core/Zoom';
+
 
 function CreateArea(props){
 
+
+    const [isExpanded, setExpanded]= useState(false);
 
 
     const [note, setNote]= useState({
@@ -23,20 +29,34 @@ function CreateArea(props){
 
     function submitNote(event){
         props.addNote(note);
+        setNote({
+            title: "",
+            content: ""
+
+        });
 
         
         event.preventDefault();
 
     }
 
+    function expand(){
+        setExpanded(true);
+    }
 
 
     return(
         <div>
-            <form>
-                <input name="title" placeholder="Title" value={note.title}  onChange={handleChange}/>
-                <textarea name="content" placeholder="Take a note..." rows="3" value={note.content} onChange={handleChange}/>
-                <button onClick={submitNote}>Add</button> 
+            <form className="create-note">
+            {isExpanded ? <input name="title" placeholder="Title" value={note.title}  onChange={handleChange}/> : null}
+                
+                <textarea onClick={expand} name="content" placeholder="Take a note..." rows={isExpanded ? 3 :1} value={note.content} onChange={handleChange}/>
+               
+               <Zoom in={isExpanded}>
+                <Fab onClick={submitNote}>
+                    <AddCircleIcon/>
+                </Fab> 
+                </Zoom>
             </form>
         </div>
     );
